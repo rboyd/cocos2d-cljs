a6033c3866c7edbfe307eb0889f67072548a61d2
Tristan Straub <tristanstraub@gmail.com>
2014-01-05 00:33:14 +1100

    Works with clojure-1.5.1. add "lein ring server" support. README updated.

--- project.clj
+++ project.clj
@@ -3,13 +3,26 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1450"]]
-  :plugins [[lein-cljsbuild "0.2.7"]]
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [org.clojure/clojurescript "0.0-2030"]
+                 [com.cemerick/piggieback "0.1.2"]]
+
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+
+  :plugins [[lein-cljsbuild "1.0.0-alpha2"]
+            [lein-ring "0.8.2"]
+            [com.cemerick/austin "0.1.3"]]
+
+  :ring {:handler my-project.handler/app}
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
   :source-paths ["src/clj"]
   :cljsbuild {
               :builds [{
-                        :source-path "src/cljs"
+                        :source-paths ["src/cljs"]
                         :compiler {
                                    :output-to "resources/public/myApp.js"
                                    :optimizations :whitespace
